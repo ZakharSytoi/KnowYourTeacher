2e38689417cb6ac6f78386a7f012e2f33d12ba8a
ZakharSytoi <zakharsytoi@gmail.com>
2023-12-13 07:49:44 +0100

    roject init commit

---
+++ src/main/java/com/example/course_project_2023/CourseProject2023Application.java
@@ -0,0 +1,13 @@
+package com.example.course_project_2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseProject2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseProject2023Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/course_project_2023/CourseProject2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.course_project_2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseProject2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
