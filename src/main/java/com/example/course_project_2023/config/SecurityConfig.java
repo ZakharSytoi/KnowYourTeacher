ce89b769119294a357774e539398a4d1a2768b3f
ZakharSytoi <zakharsytoi@gmail.com>
2023-12-14 10:51:25 +0100

    security polished and tested. Stable state achieved

---
+++ src/main/java/com/example/course_project_2023/config/SecurityConfig.java
@@ -0,0 +1,78 @@
+package com.example.course_project_2023.config;
+
+import com.example.course_project_2023.sevice.security.JwtRequestFilter;
+import com.example.course_project_2023.sevice.security.UserDetailServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static com.example.course_project_2023.sevice.security.Permission.ADMIN_SPECIFIC_PERMISSION;
+import static com.example.course_project_2023.sevice.security.Permission.USER_SPECIFIC_PERMISSION;
+
+
+@EnableWebSecurity
+@RequiredArgsConstructor
+@EnableMethodSecurity
+@Configuration
+public class SecurityConfig {
+
+    private final JwtRequestFilter jwtRequestFilter;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider(
+            UserDetailServiceImpl userService,
+            PasswordEncoder passwordEncoder
+    ) {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(userService);
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder);
+        return daoAuthenticationProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/knowyourteacher-api/v1/v1/security_checks/authorized_as_Admin_get").hasAuthority(ADMIN_SPECIFIC_PERMISSION.toString());
+                    auth.requestMatchers("/knowyourteacher-api/v1/v1/security_checks/authorized_as_Admin_post").hasAuthority(ADMIN_SPECIFIC_PERMISSION.toString());
+                    auth.requestMatchers("/knowyourteacher-api/v1/v1/security_checks/authorized_as_User_get").hasAuthority(USER_SPECIFIC_PERMISSION.toString());
+                    auth.requestMatchers("/knowyourteacher-api/v1/v1/security_checks/authorized_as_User_post").hasAuthority(USER_SPECIFIC_PERMISSION.toString());
+                    auth.anyRequest().permitAll();
+
+                })
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+//                .exceptionHandling(
+//                        ex->ex.defaultAuthenticationEntryPointFor(
+//                                new LoginUrlAuthenticationEntryPoint("/login"),
+//                                new MediaTypeRequestMatcher(MediaType.TEXT_HTML)
+//                        ))
+                .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class)
+
+                .build();
+    }
+
+}
---
+++ src/main/java/com/example/course_project_2023/web/controllers/SecurityCheckController.java
@@ -0,0 +1,51 @@
+package com.example.course_project_2023.web.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/knowyourteacher-api/v1/v1/security_checks")
+public class SecurityCheckController {
+    @GetMapping("unauthorized_get")
+    public ResponseEntity<String> unauthorizedGet() {
+        return new ResponseEntity<>("Success from unauthorized get!", HttpStatus.OK);
+    }
+
+    @PostMapping("unauthorized_post")
+    public ResponseEntity<String> unauthorizedPost(@RequestBody String someInput) {
+        return new ResponseEntity<>("Success from unauthorized post! You posted: " + someInput, HttpStatus.OK);
+    }
+
+    @GetMapping("authorized_as_Admin_get")
+    public ResponseEntity<String> authorizedAsAdminGet() {
+        return new ResponseEntity<>("Success from authorized as Admin get!", HttpStatus.OK);
+    }
+
+    @PostMapping("authorized_as_Admin_post")
+    public ResponseEntity<String> authorizedAsAdminPost(@RequestBody String someInput) {
+        return new ResponseEntity<>("Success from authorized as Admin post! You posted: " + someInput, HttpStatus.OK);
+    }
+
+    @GetMapping("authorized_as_User_get")
+    public ResponseEntity<String> authorizedAsUserGet() {
+        return new ResponseEntity<>("Success from authorized as User get!", HttpStatus.OK);
+    }
+
+    @PostMapping("authorized_as_User_post")
+    public ResponseEntity<String> authorizedAsUserPost(@RequestBody String someInput) {
+        return new ResponseEntity<>("Success from authorized as User post! You posted: " + someInput, HttpStatus.OK);
+    }
+
+    @GetMapping("role_check")
+    public ResponseEntity<String> checkRoleGet(Principal principal) {
+        if (principal != null)
+            return new ResponseEntity<>("You are logged in as: " + principal.getName(), HttpStatus.OK);
+        else return new ResponseEntity<>("You are logged in as: guest", HttpStatus.OK);
+
+    }
+}
