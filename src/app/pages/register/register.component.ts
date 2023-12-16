2f2313b2612fad1fe3c0f608cb5667563a23289c
ZakharSytoi <zakharsytoi@gmail.com>
2023-12-16 09:49:34 +0100

    login and register pages validation completed and work stable

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ShortHeaderComponent } from '../../components/short-header/short-header.component';
 import { NgStyle, NgClass } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import {
   FormGroup,
   FormControl,
@@ -11,21 +12,48 @@ import {
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [ShortHeaderComponent, NgStyle, NgClass, ReactiveFormsModule],
+  imports: [
+    ShortHeaderComponent,
+    NgStyle,
+    NgClass,
+    ReactiveFormsModule,
+    RouterModule,
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss',
 })
 export class RegisterComponent {
-
   userData: FormGroup;
 
   constructor() {
     this.userData = new FormGroup({
-      email: new FormControl('', [Validators.required, Validators.email,Validators.maxLength(30)]),
-      password: new FormControl('', [Validators.required, Validators.minLength(8), Validators.maxLength(30)]),
+      email: new FormControl('', [
+        Validators.required,
+        Validators.email,
+        Validators.maxLength(30),
+      ]),
+      nickname: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(20),
+      ]),
+      fieldOfStudies: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(50),
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(8),
+        Validators.maxLength(30),
+      ]),
+      repeatedPassword: new FormControl('', [
+        Validators.required,
+        Validators.minLength(8),
+        Validators.maxLength(30),
+      ]),
     });
   }
-
 
   onSubmit() {
     if (this.userData.valid) {
@@ -35,10 +63,50 @@ export class RegisterComponent {
     }
   }
 
-  getEmailErrorStatus() : boolean{
-    return this.userData.controls['email'].invalid &&
-    (this.userData.controls['email'].touched ||
-    this.userData.controls['email'].dirty)
+  getEmailErrorStatus(): boolean {
+    return (
+      this.userData.controls['email'].invalid &&
+      (this.userData.controls['email'].touched ||
+        this.userData.controls['email'].dirty)
+    );
   }
 
+  getNicknameErrorStatus(): boolean {
+    return (
+      this.userData.controls['nickname'].invalid &&
+      (this.userData.controls['nickname'].touched ||
+        this.userData.controls['nickname'].dirty)
+    );
+  }
+
+  getFieldOfStudiesErrorStatus(): boolean {
+    return (
+      this.userData.controls['fieldOfStudies'].invalid &&
+      (this.userData.controls['fieldOfStudies'].touched ||
+        this.userData.controls['fieldOfStudies'].dirty)
+    );
+  }
+
+  getPasswordErrorStatus(): boolean {
+    return (
+      this.userData.controls['password'].invalid &&
+      (this.userData.controls['password'].touched ||
+        this.userData.controls['password'].dirty)
+    );
+  }
+
+  getRepeatedPasswordErrorStatus(): boolean {
+    return (
+      this.userData.controls['repeatedPassword'].invalid &&
+      (this.userData.controls['repeatedPassword'].touched ||
+        this.userData.controls['repeatedPassword'].dirty)
+    );
+  }
+
+  doesPasswordsMathch(): boolean {
+    return (
+      this.userData.controls['password'].value ===
+      this.userData.controls['repeatedPassword'].value
+    );
+  }
 }
