ad07e15e65abd2cf49538377b7836d990f353387
ZakharSytoi <zakharsytoi@gmail.com>
2023-12-16 10:04:44 +0100

    implemented models for login and registration

---
+++ src/app/models/UserRegistrationRequestDto.ts
@@ -0,0 +1,8 @@
+export class UserLoginRequestDto {
+    nickname: string = '';
+    universityId: number = 1;
+    fieldOfStudies: string = '';
+    email: string = '';
+    password: string = '';
+  }
+  
