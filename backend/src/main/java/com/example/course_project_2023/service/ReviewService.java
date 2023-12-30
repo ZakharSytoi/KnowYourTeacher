dd2155e48cd45df41960a8548c85d0031d30ebf7
ZakharSytoi <zakharsytoi@gmail.com>
2023-12-30 21:26:25 +0100

    get, create and delete corresponding methods implemented. unlike and undislike remove now their functionality is in the likinUtil class

--- backend/src/main/java/com/example/course_project_2023/service/ReviewService.java
+++ backend/src/main/java/com/example/course_project_2023/service/ReviewService.java
@@ -1,28 +1,46 @@
 package com.example.course_project_2023.service;
 
+import com.example.course_project_2023.repository.daos.ReviewRepository;
 import com.example.course_project_2023.repository.daos.ReviewViewRepository;
+import com.example.course_project_2023.repository.daos.TeacherRepository;
+import com.example.course_project_2023.repository.daos.UserRepository;
+import com.example.course_project_2023.repository.model.Review;
 import com.example.course_project_2023.service.dto.ReviewDto;
+import com.example.course_project_2023.service.dto.ShortReviewDto;
+import com.example.course_project_2023.service.exception.ReviewNotFoundException;
+import com.example.course_project_2023.service.exception.TeacherNotFoundException;
+import com.example.course_project_2023.service.mappers.ReviewMapper;
 import com.example.course_project_2023.service.mappers.ReviewViewMapper;
 import com.example.course_project_2023.service.util.LikingUtil;
-import com.example.course_project_2023.service.util.UserSecurityUtil;
+import com.example.course_project_2023.service.util.UserUtil;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class ReviewService {
     private final ReviewViewRepository reviewViewRepository;
     private final ReviewViewMapper reviewViewMapper;
-    private final UserSecurityUtil userSecurityUtil;
+    private final UserUtil userSecurityUtil;
     private final LikingUtil likingUtil;
+    private final ReviewRepository reviewRepository;
+    private final UserRepository userRepository;
+    private final TeacherRepository teacherRepository;
+    private final ReviewMapper reviewMapper = ReviewMapper.INSTANCE;
 
-    public Page<ReviewDto> findAllByTeachersId(Long reviewId, int pageNumber, int pageSize) {
+    public Page<ReviewDto> findAllByTeachersId(Long teacherId, int pageNumber, int pageSize) {
         Long userId = userSecurityUtil.getUserIdFromContext();
-        return reviewViewRepository.findReviewsByTeacherId(reviewId, PageRequest.of(pageNumber, pageSize))
+        return reviewViewRepository.findReviewsByTeacherId(teacherId, PageRequest.of(pageNumber, pageSize))
                 .map((ent) -> reviewViewMapper.reviewDtoFromReviewView(ent, userId));
     }
+
 
     public void dislikeReview(Long reviewId) {
         likingUtil.dislike(userSecurityUtil.getUserIdFromContext(), reviewId);
@@ -32,12 +50,40 @@ public class ReviewService {
         likingUtil.like(userSecurityUtil.getUserIdFromContext(), reviewId);
     }
 
-    public void unlikeReview(Long reviewId) {
-        likingUtil.unlike(userSecurityUtil.getUserIdFromContext(), reviewId);
+    public ShortReviewDto getUserReviewByTeacherId(Long teacherId) {
+        Long userId = userSecurityUtil.getUserIdFromContext();
+        return reviewMapper.getShortReviewDtoFromReview(reviewRepository.findByTeacherIdAndUserId(teacherId, userId).orElseThrow(
+                () -> new ReviewNotFoundException(String.format("review from user %s on teacher %s now found", userId, teacherId))
+        ));
     }
 
-    public void undislikeReview(Long reviewId) {
-        likingUtil.undislike(userSecurityUtil.getUserIdFromContext(), reviewId);
+    public URI createUserReviewByTeacherId(Long teacherId, ShortReviewDto reviewDto) {
+        Long userId = userSecurityUtil.getUserIdFromContext();
+        Optional<Review> reviewFromDb = reviewRepository.findByTeacherIdAndUserId(teacherId, userId);
+        Review review;
+        if (reviewFromDb.isPresent()) {
+            review = reviewFromDb.get();
+            review.setScore(reviewDto.score());
+            review.setReviewText(reviewDto.reviewText());
+            review.setSubjectName(reviewDto.subjectName());
+        } else {
+            review = new Review();
+            review.setUser(userRepository.findById(userId).get());
+            review.setTeacher(teacherRepository.findById(teacherId)
+                    .orElseThrow(() -> new TeacherNotFoundException(String.format("Teacher with id %s not found", teacherId)))
+            );
+            review.setReviewText(reviewDto.reviewText());
+            review.setSubjectName(reviewDto.subjectName());
+            review.setScore(reviewDto.score());
+        }
+        return ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("/knowyourteacher-api/v1/reviews/")
+                .path(reviewRepository.save(review).getId().toString())
+                .build().toUri();
     }
 
+    @Transactional
+    public void deleteUserReviewByTeacherId(Long teacherId) {
+        reviewRepository.deleteByTeacherIdAndUserId(teacherId, userSecurityUtil.getUserIdFromContext());
+    }
 }
