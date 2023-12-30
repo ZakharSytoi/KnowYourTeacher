5ef5ac153afd696d8b74cec7be294d3ea5c09357
ZakharSytoi <zakharsytoi@gmail.com>
2023-12-30 21:30:37 +0100

    Mappings for post, delete and create methods implemented

--- backend/src/main/java/com/example/course_project_2023/web/controllers/TeacherController.java
+++ backend/src/main/java/com/example/course_project_2023/web/controllers/TeacherController.java
@@ -4,16 +4,18 @@ import com.example.course_project_2023.service.ReviewService;
 import com.example.course_project_2023.service.TeacherService;
 import com.example.course_project_2023.service.TeacherWithMostPopularReviewService;
 import com.example.course_project_2023.service.dto.ReviewDto;
+import com.example.course_project_2023.service.dto.ShortReviewDto;
 import com.example.course_project_2023.service.dto.TeacherCardDto;
 import com.example.course_project_2023.service.dto.TeacherPreviewDto;
+import com.example.course_project_2023.service.exception.ReviewNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequiredArgsConstructor
@@ -35,16 +37,38 @@ public class TeacherController {
     }
 
     @GetMapping("/{id:\\d+}")
-    public ResponseEntity<TeacherCardDto> getTeacherById(@PathVariable Long id) throws InterruptedException {
+    public ResponseEntity<TeacherCardDto> getTeacherById(@PathVariable Long id) {
         //TimeUnit.SECONDS.sleep(5);
         return ResponseEntity.ok().body(teacherService.getTeacherById(id));
     }
 
-    @GetMapping("/{id:\\d+}/reviews")
+    @GetMapping("/{teacherId:\\d+}/reviews")
     public ResponseEntity<Page<ReviewDto>> getTeacherReviews(
-            @PathVariable Long id,
+            @PathVariable Long teacherId,
             @RequestParam(required = false, defaultValue = "0") int pageNumber,
-            @RequestParam(required = false, defaultValue = "10") int pageSize){
-        return ResponseEntity.ok().body(reviewService.findAllByTeachersId(id, pageNumber, pageSize));
+            @RequestParam(required = false, defaultValue = "10") int pageSize) {
+        return ResponseEntity.ok().body(reviewService.findAllByTeachersId(teacherId, pageNumber, pageSize));
+    }
+
+    @GetMapping("/{teacherId:\\d+}/review")
+    public ResponseEntity<ShortReviewDto> getUserReviewByTeacherId(@PathVariable Long teacherId) {
+        return ResponseEntity.ok().body(reviewService.getUserReviewByTeacherId(teacherId));
+    }
+
+    @PostMapping("/{teacherId:\\d+}/review")
+    public ResponseEntity<ShortReviewDto> postUserReviewByTeacherId(@PathVariable Long teacherId,
+                                                                    @RequestBody ShortReviewDto reviewDto) {
+        return ResponseEntity.created(reviewService.createUserReviewByTeacherId(teacherId, reviewDto)).build();
+    }
+
+    @DeleteMapping("/{teacherId:\\d+}/review")
+    public ResponseEntity<?> deleteUserReviewByTeacherId(@PathVariable Long teacherId) {
+        reviewService.deleteUserReviewByTeacherId(teacherId);
+        return ResponseEntity.noContent().build();
+    }
+
+    @ExceptionHandler(ReviewNotFoundException.class)
+    ResponseEntity<?> handleReviewNotFoundException(ReviewNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
     }
 }
