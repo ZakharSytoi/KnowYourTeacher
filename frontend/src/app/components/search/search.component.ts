5e6844a1370e84e92000648b607fee1d7fc85312
ZakharSytoi <zakharsytoi@gmail.com>
2024-01-02 20:02:43 +0100

    teacher surname field added to form. university list dropdown implemented

--- frontend/src/app/components/search/search.component.ts
+++ frontend/src/app/components/search/search.component.ts
@@ -4,52 +4,75 @@ import {UniversityDto} from "../../models/UniversityDto";
 import {AuthService} from "../../services/auth.service";
 import {UniversityService} from "../../services/university.service";
 import {Router} from "@angular/router";
+import {NgFor} from '@angular/common';
 
 @Component({
-  selector: 'app-search',
-  standalone: true,
-  templateUrl: './search.component.html',
-  imports: [
-    ReactiveFormsModule
-  ],
-  styleUrl: './search.component.scss'
+    selector: 'app-search',
+    standalone: true,
+    templateUrl: './search.component.html',
+    imports: [
+        ReactiveFormsModule,
+        NgFor
+    ],
+    styleUrl: './search.component.scss'
 })
 export class SearchComponent {
     searchForm: FormGroup
     universityList: UniversityDto[];
 
-  constructor(
-      private authService: AuthService,
-      private universityService: UniversityService,
-  ) {
-    this.universityList = [];
-    universityService.getUniversitiesList().subscribe({
-      next: universitiesList => {
-        this.universityList = universitiesList;
-      },
-    });
+    constructor(
+        private authService: AuthService,
+        private universityService: UniversityService,
+        private router: Router
+    ) {
+        this.universityList = [];
+        universityService.getUniversitiesList().subscribe({
+            next: universitiesList => {
+                this.universityList = universitiesList;
+            },
+        });
 
-    this.searchForm = new FormGroup({
-      teacherName: new FormControl("", [
-        Validators.required,
-        Validators.minLength(3),
-        Validators.maxLength(100),
-      ]),
-      subject: new FormControl("", [
-        Validators.required,
-        Validators.minLength(3),
-        Validators.maxLength(50),
-      ]),
-      university: new FormControl("Choose University", [
-        Validators.required,
-      ]),
-
-    });
-  }
-
-  onSubmit() {
-    if(this.searchForm.valid){
-
+        this.searchForm = new FormGroup({
+            teacherName: new FormControl("", [
+                Validators.minLength(3),
+                Validators.maxLength(100),
+            ]),
+            teacherSurname: new FormControl("", [
+                Validators.minLength(3),
+                Validators.maxLength(100),
+            ]),
+            subject: new FormControl("", [
+                Validators.minLength(3),
+                Validators.maxLength(50),
+            ]),
+            universityId: new FormControl("University"),
+        });
     }
-  }
+
+    onSubmit() {
+        if (this.searchForm.valid) {
+            this.router.navigate(['/search'],
+                {
+                    queryParams:
+                        {
+                            teacherName: this.searchForm.controls["teacherName"].value,
+                            teacherSurname: this.searchForm.controls["teacherSurname"].value,
+                            subject: this.searchForm.controls["subject"].value,
+                            universityId: this.searchForm.controls["universityId"].value
+                                === "universityId" ? "" : this.searchForm.controls["universityId"].value
+                        }
+                });
+        } else {
+            console.log(this.searchForm.errors)
+        }
+    }
+
+    getUniversityErrorStatus(): boolean {
+        if (
+            this.searchForm.controls["university"].value === "University"
+        ) {
+            return true;
+        }
+        return false;
+    }
 }
