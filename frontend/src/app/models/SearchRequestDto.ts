581665aa54bd39693d6f2f9c069ac3b28025c45f
ZakharSytoi <zakharsytoi@gmail.com>
2024-01-03 15:30:21 +0100

    search request dto now class for more precise work

--- frontend/src/app/models/SearchRequestDto.ts
+++ frontend/src/app/models/SearchRequestDto.ts
@@ -1,6 +1,39 @@
-export interface SearchRequestDto{
-    teacherName?: string,
-    teacherSurname?: string,
-    subject?: string,
-    universityId?: number
+export class SearchRequestDto{
+    private _teacherName?: string;
+    private _teacherSurname?: string;
+    private _subject?: string;
+    private _universityId?: string;
+
+
+    set teacherName(value: string) {
+        this._teacherName = value;
+    }
+
+    set teacherSurname(value: string) {
+        this._teacherSurname = value;
+    }
+
+    set subject(value: string) {
+        this._subject = value;
+    }
+
+    set universityId(value: string) {
+        this._universityId = value;
+    }
+
+    get teacherName(): string {
+        return <string>this._teacherName;
+    }
+
+    get teacherSurname(): string {
+        return <string>this._teacherSurname;
+    }
+
+    get subject(): string {
+        return <string>this._subject;
+    }
+
+    get universityId(): string {
+        return <string>this._universityId;
+    }
 }
