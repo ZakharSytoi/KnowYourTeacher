457a438b8b5f05a5981ea821ac19746017cad5e4
ZakharSytoi <zakharsytoi@gmail.com>
2024-01-03 15:28:17 +0100

    search service fixed wrong url bug

--- frontend/src/app/services/search.service.ts
+++ frontend/src/app/services/search.service.ts
@@ -2,24 +2,24 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {SearchRequestDto} from "../models/SearchRequestDto";
 import {Observable} from "rxjs";
-import {TeacherPreviewDto} from "../models/TeacherPreviewDto";
 import {Page} from "../models/Page";
 import {environment} from "../../environments/environment";
+import {SearchedReviewDto} from "../models/SearchedReviewDto";
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class SearchService {
 
-  constructor(private readonly http: HttpClient) { }
+    constructor(private readonly http: HttpClient) {
+    }
 
-  search$ = (searchRequest: SearchRequestDto, pageNumber: number = 0, pageSize: number = 10): Observable<Page<TeacherPreviewDto>> =>
-      this.http.get<Page<TeacherPreviewDto>>(`${environment.BASE_API_URL}teachers/search
-      ?pageNumber=${pageNumber}
-      &pageSize=${pageSize}}
-      &teacherName=${searchRequest.teacherName}
-      &teacherSurname=${searchRequest.teacherSurname}
-      &subject=${searchRequest.subject}
-      &universityId=${searchRequest.universityId}`);
-
+    search$ = (searchRequest: SearchRequestDto, pageNumber: number = 0, pageSize: number = 10): Observable<Page<SearchedReviewDto>> => {
+        let requestParams: string = ''
+        if (searchRequest.teacherName) requestParams = requestParams + '&teacherName=' + searchRequest.teacherName.trim();
+        if (searchRequest.teacherSurname) requestParams = requestParams + '&teacherSurname=' + searchRequest.teacherSurname.trim();
+        if (searchRequest.subject) requestParams = requestParams + '&subject=' + searchRequest.subject.trim();
+        if (searchRequest.universityId && !isNaN(parseInt(searchRequest.universityId))) requestParams = requestParams + '&universityId=' + searchRequest.universityId.trim();
+        return this.http.get<Page<SearchedReviewDto>>(`${environment.BASE_API_URL}reviews?pageNumber=${pageNumber}&pageSize=${pageSize}${requestParams}`)
+    }
 }
