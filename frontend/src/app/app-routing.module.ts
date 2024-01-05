0cc7f94efbd49e8f9b12e9659947a25078504bff
ZakharSytoi <zakharsytoi@gmail.com>
2024-01-05 15:38:28 +0100

    guides and teacher creation pages created. routing adjusted

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,46 +1,59 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from "./pages/home/home.component";
 import {RegisterComponent} from "./pages/register/register.component";
 import {LoginComponent} from "./pages/login/login.component";
 import {TeacherPageComponent} from "./pages/teacher-page/teacher-page.component";
 import {SearchComponent} from "./pages/search/search.component";
+import {GuidesComponent} from "./pages/guides/guides.component";
+import {TeacherCreationComponent} from "./pages/teacher-creation/teacher-creation.component";
 
 const routes: Routes = [
-  {
-    path: '',
-    component: HomeComponent,
-    title: 'KnowYourTeacher'
-  },
-  {
-    path: 'login',
-    component: LoginComponent,
-    title: 'Login'
-  },
-  {
-    path: 'register',
-    component: RegisterComponent,
-    title: 'Registration'
-  },
-  {
-    path: 'teacher/:id',
-    component: TeacherPageComponent,
-    title: 'KnowYourTeacher'
-  },
-  {
-    path: 'search',
-    component: SearchComponent,
-    title: 'KnowYourTeacher'
-  },
-  {
-    path: '**',
-    component: HomeComponent,
-    redirectTo: ''
-  }
+    {
+        path: '',
+        component: HomeComponent,
+        title: 'KnowYourTeacher'
+    },
+    {
+        path: 'login',
+        component: LoginComponent,
+        title: 'Login'
+    },
+    {
+        path: 'register',
+        component: RegisterComponent,
+        title: 'Registration'
+    },
+    {
+        path: 'teacher/create',
+        component: TeacherCreationComponent,
+        title: 'KnowYourTeacher'
+    },
+    {
+        path: 'teacher/:id',
+        component: TeacherPageComponent,
+        title: 'KnowYourTeacher'
+    },
+    {
+        path: 'search',
+        component: SearchComponent,
+        title: 'KnowYourTeacher'
+    },
+    {
+        path: 'guides',
+        component: GuidesComponent,
+        title: 'KnowYourTeacher'
+    },
+    {
+        path: '**',
+        component: HomeComponent,
+        redirectTo: ''
+    }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
---
+++ frontend/src/app/pages/teacher-creation/teacher-creation.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-teacher-creation',
+  standalone: true,
+  imports: [],
+  templateUrl: './teacher-creation.component.html',
+  styleUrl: './teacher-creation.component.scss'
+})
+export class TeacherCreationComponent {
+
+}
