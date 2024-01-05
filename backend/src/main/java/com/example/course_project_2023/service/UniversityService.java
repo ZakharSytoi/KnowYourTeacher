a521770115ad2f3a9095fec44fa5aed175961180
ZakharSytoi <zakharsytoi@gmail.com>
2024-01-05 14:06:48 +0100

    controller mapping for university by id implemented

--- backend/src/main/java/com/example/course_project_2023/service/UniversityService.java
+++ backend/src/main/java/com/example/course_project_2023/service/UniversityService.java
@@ -2,6 +2,7 @@ package com.example.course_project_2023.service;
 
 import com.example.course_project_2023.repository.daos.UniversityRepository;
 import com.example.course_project_2023.service.dto.UniversityDto;
+import com.example.course_project_2023.service.exception.UniversityNotFoundException;
 import com.example.course_project_2023.service.mappers.UniversityMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -15,7 +16,13 @@ public class UniversityService {
     private final UniversityRepository universityRepository;
     private final UniversityMapper universityMapper = UniversityMapper.INSTANCE;
 
-    public List<UniversityDto> getAllUniversities(){
+    public List<UniversityDto> getAllUniversities() {
         return universityMapper.universityListToUniversityDtoList(universityRepository.findAll());
+    }
+
+    public UniversityDto getUniversityById(Long id) {
+        return universityMapper.universityToUniversityDto(universityRepository.findById(id).orElseThrow(
+                () -> new UniversityNotFoundException(String.format("University with id %s not found", id)))
+        );
     }
 }
--- backend/src/main/java/com/example/course_project_2023/web/controllers/UniversityController.java
+++ backend/src/main/java/com/example/course_project_2023/web/controllers/UniversityController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +21,9 @@ public class UniversityController {
     @GetMapping()
     public ResponseEntity<List<UniversityDto>> getAllUniversities() {
         return new ResponseEntity<>(universityService.getAllUniversities(), HttpStatus.OK);
+    }
+    @GetMapping("/{id:\\d+}")
+    public ResponseEntity<UniversityDto> getUniversityById(@PathVariable Long id) {
+        return new ResponseEntity<>(universityService.getUniversityById(id), HttpStatus.OK);
     }
 }
