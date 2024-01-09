7341a91c6baafa695dac6f3b851c679edd9eae54
ZakharSytoi <zakharsytoi@gmail.com>
2024-01-09 19:27:24 +0100

    SecurityConfig updated to fit new application address

--- backend/src/main/java/com/example/course_project_2023/config/SecurityConfig.java
+++ backend/src/main/java/com/example/course_project_2023/config/SecurityConfig.java
@@ -59,7 +59,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "http://localhost:8077", "http://localhost:80", "http://3.70.177.167"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "http://localhost:8077", "http://localhost:80", "https://knowyourteacher.online"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.addExposedHeader("Location");
