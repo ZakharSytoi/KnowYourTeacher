fe366a4f71875cec3d740b4af9fa8eee234b60f0
ZakharSytoi <zakharsytoi@gmail.com>
2024-01-09 19:25:55 +0100

    base link updated

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,6 @@
 
 export const environment = {
     production: true,
-    BASE_API_URL: 'https://3.70.177.167:82/knowyourteacher-api/v1/'
+    BASE_API_URL: 'https://knowyourteacher.online:82/knowyourteacher-api/v1/'
+    //BASE_API_URL: 'https://3.70.177.167:82/knowyourteacher-api/v1/'
 };
