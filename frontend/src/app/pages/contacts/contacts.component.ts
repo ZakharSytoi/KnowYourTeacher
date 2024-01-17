3ccb71d57140f66a79788ac2d38984bbdd9e6f19
ZakharSytoi <zakharsytoi@gmail.com>
2024-01-17 18:34:32 +0100

    contacts page implemented

---
+++ frontend/src/app/pages/contacts/contacts.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {HeaderComponent} from "../../components/header/header.component";
+
+@Component({
+  selector: 'app-contacts',
+  standalone: true,
+    imports: [
+        HeaderComponent
+    ],
+  templateUrl: './contacts.component.html',
+  styleUrl: './contacts.component.scss'
+})
+export class ContactsComponent {
+
+}
