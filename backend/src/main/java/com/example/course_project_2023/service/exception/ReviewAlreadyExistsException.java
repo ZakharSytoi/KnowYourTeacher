5f7267a5e88ad475c40f946c57bde2d1164e2996
ZakharSytoi <zakharsytoi@gmail.com>
2024-01-19 17:31:35 +0100

    review already exists exception deleted for not being used

--- backend/src/main/java/com/example/course_project_2023/service/exception/ReviewAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.example.course_project_2023.service.exception;
-
-public class ReviewAlreadyExistsException extends RuntimeException{
-    public ReviewAlreadyExistsException(String message) {
-        super(message);
-    }
-}
