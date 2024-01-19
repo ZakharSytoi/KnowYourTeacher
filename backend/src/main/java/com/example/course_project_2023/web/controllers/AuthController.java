fac004e9ed4f0804c8c8eb8cbf596bd7c8176641
ZakharSytoi <zakharsytoi@gmail.com>
2024-01-19 15:55:13 +0100

    method parameters validation turned on

--- backend/src/main/java/com/example/course_project_2023/web/controllers/AuthController.java
+++ backend/src/main/java/com/example/course_project_2023/web/controllers/AuthController.java
@@ -6,24 +6,27 @@ import com.example.course_project_2023.service.dto.UserLoginDtoRequest;
 import com.example.course_project_2023.service.dto.UserRegisterDtoRequest;
 import com.example.course_project_2023.service.exception.UserWithEmailAlreadyExistsException;
 import com.example.course_project_2023.service.exception.UserWithNicknameAlreadyExistsException;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/knowyourteacher-api/v1")
+@Validated
 public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody UserLoginDtoRequest loginUserDto) {
+    public ResponseEntity<?> login(@Valid @RequestBody UserLoginDtoRequest loginUserDto) {
         return ResponseEntity.ok(new JwtResponse(authService.authenticateUser(loginUserDto)));
     }
 
     @PostMapping("/register")
-    public ResponseEntity<?> register(@RequestBody UserRegisterDtoRequest userDtoRequest) throws UserWithEmailAlreadyExistsException, UserWithNicknameAlreadyExistsException {
+    public ResponseEntity<?> register(@Valid @RequestBody UserRegisterDtoRequest userDtoRequest) throws UserWithEmailAlreadyExistsException, UserWithNicknameAlreadyExistsException {
         authService.registerUser(userDtoRequest);
         return ResponseEntity.status(HttpStatusCode.valueOf(200)).build();
     }
