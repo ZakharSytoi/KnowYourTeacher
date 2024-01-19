2a22ecfaf4ddd8c3909544db91e84f07300f73ed
ZakharSytoi <zakharsytoi@gmail.com>
2024-01-19 11:46:04 +0100

    validation added to register request

--- backend/src/main/java/com/example/course_project_2023/service/dto/UserRegisterDtoRequest.java
+++ backend/src/main/java/com/example/course_project_2023/service/dto/UserRegisterDtoRequest.java
@@ -1,9 +1,28 @@
 package com.example.course_project_2023.service.dto;
 
-public record UserRegisterDtoRequest(String nickname,
-                                     Long universityId,
-                                     String fieldOfStudies,
-                                     String email,
-                                     String password
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.Length;
+
+public record UserRegisterDtoRequest(
+        @NotNull
+        @Length(min = 6, max = 30)
+        String nickname,
+        @NotNull
+        @Min(0)
+        @Max(10000)
+        Long universityId,
+        @NotNull
+        @Length(min = 3, max = 50)
+        String fieldOfStudies,
+        @NotNull
+        @Email
+        @Length(max = 60)
+        String email,
+        @NotNull
+        @Length(min = 8, max = 30)
+        String password
 ) {
 }
