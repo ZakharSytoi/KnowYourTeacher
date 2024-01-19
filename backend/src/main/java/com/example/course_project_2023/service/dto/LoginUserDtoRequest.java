abcecbeea49170b99c04e64b6ab191ca4f4abd3a
ZakharSytoi <zakharsytoi@gmail.com>
2024-01-19 11:23:46 +0100

    validation added

--- backend/src/main/java/com/example/course_project_2023/service/dto/LoginUserDtoRequest.java
+++ backend/src/main/java/com/example/course_project_2023/service/dto/LoginUserDtoRequest.java
@@ -1,9 +1,18 @@
 package com.example.course_project_2023.service.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 @Data
 public class LoginUserDtoRequest {
+    @NotNull
+    @Email
+    @Length(max = 30)
     private String username;
+    @NotNull
+    @Email
+    @Length(max = 30)
     private String password;
 }
