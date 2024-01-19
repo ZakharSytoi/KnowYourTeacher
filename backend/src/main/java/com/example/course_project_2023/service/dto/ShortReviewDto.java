1b822db71ad3d0c4496201166a75cda3c832f7c5
ZakharSytoi <zakharsytoi@gmail.com>
2024-01-19 17:25:20 +0100

    ShortReviewDto validation added

--- backend/src/main/java/com/example/course_project_2023/service/dto/ShortReviewDto.java
+++ backend/src/main/java/com/example/course_project_2023/service/dto/ShortReviewDto.java
@@ -1,8 +1,20 @@
 package com.example.course_project_2023.service.dto;
 
-public record ShortReviewDto (
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.Length;
+
+public record ShortReviewDto(
+        @NotNull(message = "score can not be Null")
+        @Min(value = 1, message = "score value must be greater then or equal 1")
+        @Max(value = 5, message = "score value must not exceed 5")
         short score,
+        @NotNull(message = "subjectName can not be Null")
+        @Length(min = 3, max = 50, message = "subjectName length must be between 3 and 50 characters")
         String subjectName,
+        @NotNull(message = "reviewText can not be Null")
+        @Length(min = 3, max = 1000, message = "reviewText length must be between 3 and 1000 characters")
         String reviewText
 ) {
 }
