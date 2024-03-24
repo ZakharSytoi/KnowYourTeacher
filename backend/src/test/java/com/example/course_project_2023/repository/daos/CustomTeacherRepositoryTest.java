f5afdc7d6e5e3351d9fe10337459311a91d697b5
ZakharSytoi <zakharsytoi@gmail.com>
2024-03-24 13:31:48 +0100

    tests for method returning teacher id's added

--- backend/src/test/java/com/example/course_project_2023/repository/daos/CustomTeacherRepositoryTest.java
+++ backend/src/test/java/com/example/course_project_2023/repository/daos/CustomTeacherRepositoryTest.java
@@ -94,4 +94,70 @@ public class CustomTeacherRepositoryTest {
 
         Assertions.assertEquals(teacherRepository.findByParams(params).size(), 1);
     }
+
+    @Test
+    void findByParamsReturnIds_EmptyArgs_RetrieveAll() {
+        Assertions.assertEquals(teacherRepository.findByParamsReturnIds(new HashMap<>()).size(), 9);
+    }
+
+    @Test
+    void findByParamsReturnIds_NameGiven_RetrieveOne() {
+        params.put("teacherName", "Joh");
+
+        Assertions.assertEquals(teacherRepository.findByParamsReturnIds(params).size(), 1);
+    }
+
+    @Test
+    void findByParamsReturnIds_SurnameGiven_RetrieveOne() {
+        params.put("teacherSurname", "doe");
+
+        Assertions.assertEquals(teacherRepository.findByParamsReturnIds(params).size(), 1);
+    }
+
+    @Test
+    void findByParamsReturnIds_SubjectGiven_RetrieveOne() {
+        params.put("subject", "Math");
+
+        Assertions.assertEquals(teacherRepository.findByParamsReturnIds(params).size(), 1);
+    }
+
+
+    @Test
+    void findByParamsReturnIds_AllParamsGiven_RetrieveOne() {
+        params.put("teacherSurname", "doe");
+        params.put("teacherName", "jo");
+        params.put("universityId", "2");
+
+        Assertions.assertEquals(teacherRepository.findByParamsReturnIds(params).size(), 1);
+    }
+
+    @Test
+    void findByParamsReturnIds_AllParamsGivenInDifferentCases_RetrieveOne() {
+        params.put("teacherSurname", "dOe");
+        params.put("teacherName", "jO");
+        params.put("universityId", "2");
+
+        Assertions.assertEquals(teacherRepository.findByParamsReturnIds(params).size(), 1);
+    }
+
+    @Test
+    void findByParamsReturnIds_AllParamsGivenInDifferentCasesForJohn_RetrieveExactlyJohn() {
+        params.put("teacherSurname", "dOe");
+        params.put("teacherName", "jO");
+        params.put("universityId", "2");
+
+        Long retrievedId = teacherRepository.findByParamsReturnIds(params).get(0);
+
+        Assertions.assertEquals(retrievedId, 1L);
+    }
+
+
+    @Test
+    void findByParamsReturnIds_AllParamsGivenForTeacherNotExist_RetrieveZero() {
+        params.put("teacherSurname", "dOe");
+        params.put("teacherName", "jO");
+        params.put("universityId", "2");
+
+        Assertions.assertEquals(teacherRepository.findByParamsReturnIds(params).size(), 1);
+    }
 }
