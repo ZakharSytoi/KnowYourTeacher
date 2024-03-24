7a69a6dfd5dd919a55318b74857cf2693170962c
ZakharSytoi <zakharsytoi@gmail.com>
2024-03-24 14:14:41 +0100

    TeacherWithMostPopularReviewRepository now implements Fragment repository

--- backend/src/main/java/com/example/course_project_2023/repository/daos/TeacherWithMostPopularReviewRepository.java
+++ backend/src/main/java/com/example/course_project_2023/repository/daos/TeacherWithMostPopularReviewRepository.java
@@ -1,7 +1,9 @@
 package com.example.course_project_2023.repository.daos;
 
+import com.example.course_project_2023.repository.daos.customRepositories.CustomTeacherWithMostPopularReviewRepository;
 import com.example.course_project_2023.repository.model.views.TeacherWithMostPopularReview;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TeacherWithMostPopularReviewRepository extends ReadOnlyRepository<TeacherWithMostPopularReview, Long>{
+public interface TeacherWithMostPopularReviewRepository extends JpaRepository<TeacherWithMostPopularReview, Long>, CustomTeacherWithMostPopularReviewRepository {
 
 }
