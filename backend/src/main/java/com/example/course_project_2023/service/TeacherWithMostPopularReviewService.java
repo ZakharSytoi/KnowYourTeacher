8c48cbbd2de5e56931fd363a445c722ea433b3ce
ZakharSytoi <zakharsytoi@gmail.com>
2024-03-24 15:47:12 +0100

    service method for TeacherWithMostPopularReview parametrized search implemented

--- backend/src/main/java/com/example/course_project_2023/service/TeacherWithMostPopularReviewService.java
+++ backend/src/main/java/com/example/course_project_2023/service/TeacherWithMostPopularReviewService.java
@@ -4,10 +4,12 @@ import com.example.course_project_2023.repository.daos.TeacherWithMostPopularRev
 import com.example.course_project_2023.service.dto.TeacherPreviewDto;
 import com.example.course_project_2023.service.mappers.TeacherWithMostPopularReviewMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -18,5 +20,10 @@ public class TeacherWithMostPopularReviewService {
 
     public List<TeacherPreviewDto> getNTeachersWithMostPopularReviews(int n){
         return teacherMapper.teacherListToTeacherDtoResponseList(teacherRepository.findAll(PageRequest.of(0, 10)).getContent());
+    }
+
+    public Page<TeacherPreviewDto> findBySearchParams(int pageNumber, int pageSize, Map<String, String> searchParams) {
+        return teacherRepository.findByParams(searchParams, PageRequest.of(pageNumber, pageSize))
+                .map(teacherMapper::teacherToTeacherDtoResponse);
     }
 }
