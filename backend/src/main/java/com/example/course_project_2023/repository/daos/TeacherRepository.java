8f9874c47cb684ba58fffaefdc5a45f9498b0e89
ZakharSytoi <zakharsytoi@gmail.com>
2024-03-24 09:02:50 +0100

    Fragment Repository added to Teacher Repository

--- backend/src/main/java/com/example/course_project_2023/repository/daos/TeacherRepository.java
+++ backend/src/main/java/com/example/course_project_2023/repository/daos/TeacherRepository.java
@@ -1,7 +1,8 @@
 package com.example.course_project_2023.repository.daos;
 
+import com.example.course_project_2023.repository.daos.customRepositories.CustomTeacherRepository;
 import com.example.course_project_2023.repository.model.Teacher;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TeacherRepository extends JpaRepository<Teacher, Long> {
+public interface TeacherRepository extends JpaRepository<Teacher, Long>, CustomTeacherRepository {
 }
