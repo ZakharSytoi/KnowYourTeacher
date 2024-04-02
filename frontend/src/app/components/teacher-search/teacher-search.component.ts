39f31a9d58619381838a727ae0b8159a0d90f022
ZakharSytoi <zakharsytoi@gmail.com>
2024-04-02 20:17:18 +0200

    teacher search now utilizes pagination logic

--- frontend/src/app/components/teacher-search/teacher-search.component.ts
+++ frontend/src/app/components/teacher-search/teacher-search.component.ts
@@ -1,16 +1,15 @@
-import {Component, Input, OnChanges, OnDestroy, OnInit} from '@angular/core';
+import {Component, Input, OnChanges} from '@angular/core';
 import {AsyncPipe, NgClass, NgForOf, NgIf, NgStyle, NgSwitch, NgSwitchCase} from "@angular/common";
-import {ActivatedRoute, RouterLink} from "@angular/router";
-import {catchError, map, Observable, of, startWith, Subscription} from "rxjs";
+import {RouterLink} from "@angular/router";
+import {catchError, map, Observable, of, startWith} from "rxjs";
 import {Page} from "../../models/Page";
-import {HttpClient, HttpErrorResponse} from "@angular/common/http";
+import {HttpErrorResponse} from "@angular/common/http";
 import {TeacherPreviewDto} from "../../models/TeacherPreviewDto";
 import {SearchService} from "../../services/search.service";
-import {UniversityService} from "../../services/university.service";
-import {AuthService} from "../../services/auth.service";
 import {SearchRequestDto} from "../../models/SearchRequestDto";
 import {ErrorComponent} from "../error/error.component";
 import {TeacherPreviewComponent} from "../teacher-preview/teacher-preview.component";
+import {PaginationComponent} from "../pagination/pagination.component";
 
 @Component({
     selector: 'app-teacher-search',
@@ -26,7 +25,8 @@ import {TeacherPreviewComponent} from "../teacher-preview/teacher-preview.compon
         ErrorComponent,
         NgClass,
         NgStyle,
-        TeacherPreviewComponent
+        TeacherPreviewComponent,
+        PaginationComponent
     ],
     styleUrl: './teacher-search.component.scss'
 })
@@ -36,21 +36,12 @@ export class TeacherSearchComponent implements OnChanges {
 
     };
     search$!: Observable<{ searchState: string, teachersPage?: Page<TeacherPreviewDto>, error?: HttpErrorResponse }>
-    currentRange!: { pagesToTheLeft: boolean, pagesToTheRight: boolean, range: number[], currentPage: number }
 
-    constructor(private readonly searchService: SearchService,
-                protected readonly route: ActivatedRoute,
-                private readonly universityService: UniversityService,
-                readonly authService: AuthService,
-                private readonly http: HttpClient) {
-
-    }
+    constructor(private readonly searchService: SearchService) {}
 
     loadPage(pageNumber: number = 0){
         this.search$ = this.searchService.searchTeachers$(this.params, pageNumber).pipe(
             map((response: Page<TeacherPreviewDto>) => {
-                this.currentRange = this.getCurrentRange(response.totalPages, response.number);
-
                 return ({searchState: 'LOADED', teachersPage: response})
             }),
             startWith({searchState: 'LOADING'}),
@@ -62,43 +53,5 @@ export class TeacherSearchComponent implements OnChanges {
         this.loadPage()
     }
 
-    range = (start: number, stop: number) =>
-        Array.from({length: (stop - start)}, (_, i) => start + i);
 
-    getCurrentRange(totalPages: number, currentPage: number) {
-        if (totalPages <= 10) {
-            return {
-                pagesToTheLeft: false,
-                pagesToTheRight: false,
-                range: this.range(0, totalPages),
-                currentPage: currentPage
-            }
-        } else {
-            let right = 0;
-            let left = 0;
-            let pagesToTheLeft = true;
-            let pagesToTheRight = true;
-            if (totalPages >= 10) {
-                right = currentPage + 5
-                left = currentPage - 4;
-                if (left <= 0) {
-                    right = currentPage + 5 - left;
-                    left = 0;
-                    pagesToTheLeft = false;
-                } else if (right >= totalPages) {
-                    left = currentPage - right + totalPages - 4;
-                    right = totalPages
-                    pagesToTheRight = false;
-                }
-            }
-            return {
-                pagesToTheLeft: pagesToTheLeft,
-                pagesToTheRight: pagesToTheRight,
-                range: this.range(left, right),
-                currentPage: currentPage
-            }
-        }
-    }
-
-    protected readonly JSON = JSON;
 }
