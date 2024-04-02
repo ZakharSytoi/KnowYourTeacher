0961ae55f2496352a822fb3897afdf1ad43a476a
ZakharSytoi <zakharsytoi@gmail.com>
2024-04-02 18:37:47 +0200

    top teachers component now utilizes new teacher preview component

--- frontend/src/app/components/topteachers/top-teachers.component.ts
+++ frontend/src/app/components/topteachers/top-teachers.component.ts
@@ -1,34 +1,43 @@
-import {AfterViewChecked, AfterViewInit, Component} from '@angular/core';
-import {NgStyle, NgClass, NgFor} from "@angular/common";
+import {Component, OnInit} from '@angular/core';
+import {AsyncPipe, NgFor, NgIf, NgSwitch, NgSwitchCase} from "@angular/common";
 
 
 import {TeacherPreviewDto} from "../../models/TeacherPreviewDto";
 import {RouterLink} from "@angular/router";
 import {TeacherService} from "../../services/teacher.service";
+import {TeacherPreviewComponent} from "../teacher-preview/teacher-preview.component";
+import {ErrorComponent} from "../error/error.component";
+import {catchError, map, Observable, of, startWith} from "rxjs";
+import {HttpErrorResponse} from "@angular/common/http";
 
 declare function initRatings(): void;
 
 @Component({
     selector: 'app-topteachers',
     standalone: true,
-    imports: [NgFor, RouterLink],
+    imports: [NgFor, RouterLink, TeacherPreviewComponent, AsyncPipe, ErrorComponent, NgIf, NgSwitchCase, NgSwitch],
     templateUrl: './top-teachers.component.html',
     styleUrl: './top-teachers.component.scss'
 })
-export class TopTeachersComponent {
+export class TopTeachersComponent implements OnInit {
 
-    topTeachersList: TeacherPreviewDto[];
+    topTeachers$!: Observable<{ state: string, teachers?: TeacherPreviewDto[], error?: HttpErrorResponse }>
 
     constructor(
         private topTeacherService: TeacherService
     ) {
-        this.topTeachersList = [];
-        topTeacherService.getTopTeachersList().subscribe({
-            next: (topTeachersList) => {
-                this.topTeachersList = topTeachersList;
-            }
-        })
     }
+
+    ngOnInit(): void {
+        this.topTeachers$ = this.topTeacherService.topTeachers$().pipe(
+            map((response: TeacherPreviewDto[]) => {
+                return {state: 'LOADED', teachers: response}
+            }),
+            startWith({state: 'LOADING'}),
+            catchError(error => of({state: 'ERROR', error: error}))
+        );
+    }
+
 
     ngAfterViewChecked(): void {
         initRatings();
