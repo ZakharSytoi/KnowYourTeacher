27b5b3bb8430ab51c7221cdb378fb1992b1c3105
ZakharSytoi <zakharsytoi@gmail.com>
2024-04-14 14:24:43 +0200

    implement controller for new like/dislike system

---
+++ backend/src/main/java/com/example/course_project_2023/web/controllers/LikingController.java
@@ -0,0 +1,39 @@
+package com.example.course_project_2023.web.controllers;
+
+import com.example.course_project_2023.service.ReviewService;
+import com.example.course_project_2023.service.dto.DislikeOnReviewResponseDto;
+import com.example.course_project_2023.service.dto.LikeOnReviewResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/knowyourteacher-api/v2/reviews")
+public class LikingController {
+
+    private final ReviewService reviewService;
+
+    @PostMapping("/{id:\\d+}/like")
+    public ResponseEntity<LikeOnReviewResponseDto> likeReview(@PathVariable Long id) {
+        return ResponseEntity.ok().body(reviewService.likeReviewV2(id));
+    }
+
+    @PostMapping("/{id:\\d+}/unlike")
+    public ResponseEntity<LikeOnReviewResponseDto> unLikeReview(@PathVariable Long id) {
+        return ResponseEntity.ok().body(reviewService.unLikeReviewV2(id));
+    }
+
+    @PostMapping("/{id:\\d+}/dislike")
+    public ResponseEntity<DislikeOnReviewResponseDto> dislikeReview(@PathVariable Long id) {
+        return ResponseEntity.ok().body(reviewService.dislikeReviewV2(id));
+    }
+
+    @PostMapping("/{id:\\d+}/undislike")
+    public ResponseEntity<DislikeOnReviewResponseDto> unDislikeReview(@PathVariable Long id) {
+        return ResponseEntity.ok().body(reviewService.unDislikeReviewV2(id));
+    }
+}
