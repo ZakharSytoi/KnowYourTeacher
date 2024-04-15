2f0da1fe31e9660fd4648cba41663153cd9082cd
ZakharSytoi <zakharsytoi@gmail.com>
2024-04-15 11:30:13 +0200

    update env with v2 api url

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,5 +1,6 @@
 
 export const environment = {
     production: false,
-    BASE_API_URL: 'https://localhost:8082/knowyourteacher-api/v1/'
+    BASE_API_URL: 'https://localhost:8082/knowyourteacher-api/v1/',
+    BASE_API_URL_V2: 'https://localhost:8082/knowyourteacher-api/v2/'
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,7 @@
 
 export const environment = {
     production: true,
-    BASE_API_URL: 'https://knowyourteacher.online:82/knowyourteacher-api/v1/'
+    BASE_API_URL: 'https://knowyourteacher.online:82/knowyourteacher-api/v1/',
+    BASE_API_URL_V2: 'https://knowyourteacher.online:82/knowyourteacher-api/v2/'
     //BASE_API_URL: 'https://3.70.177.167:82/knowyourteacher-api/v1/'
 };
