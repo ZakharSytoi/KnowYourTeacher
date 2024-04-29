ea10776dc1a51e69e0fed96bb111e75b91fc6dd9
ZakharSytoi <zakharsytoi@gmail.com>
2024-04-29 06:56:22 +0200

    implement basic UserService

--- backend/src/main/java/com/example/course_project_2023/service/UserService.java
+++ backend/src/main/java/com/example/course_project_2023/service/UserService.java
@@ -1,14 +1,22 @@
 package com.example.course_project_2023.service;
 
 import com.example.course_project_2023.repository.daos.UserRepository;
+import com.example.course_project_2023.service.dto.UserResponseDto;
+import com.example.course_project_2023.service.mappers.UserMapper;
+import com.example.course_project_2023.service.util.UserUtil;
 import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+    private final UserUtil userSecurityUtil;
+
+    public UserResponseDto getUserInfo() {
+        Long userId = userSecurityUtil.getUserIdFromContext();
+        return UserMapper.INSTANCE.userToUserResponseDto(userRepository.findById(userId).get());
+    }
 
     boolean existsWithNickname(String nickname) {
         return userRepository.existsByNickname(nickname);
