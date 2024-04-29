ce97d538f918206b95a1d22f0527539f58236206
ZakharSytoi <zakharsytoi@gmail.com>
2024-04-29 07:21:16 +0200

    implement PasswordUpdateDtoRequest record

---
+++ backend/src/main/java/com/example/course_project_2023/service/dto/PasswordUpdateDtoRequest.java
@@ -0,0 +1,11 @@
+package com.example.course_project_2023.service.dto;
+
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.Length;
+
+public record PasswordUpdateDtoRequest(
+        @NotNull(message = "password can not be null")
+        @Length(min = 8, max = 30, message = "password length must be between 8 and 30 characters")
+        String password
+) {
+}
