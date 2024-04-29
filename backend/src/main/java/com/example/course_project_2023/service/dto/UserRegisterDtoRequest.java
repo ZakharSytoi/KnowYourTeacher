06a7edc8225ce195080078a442d91c3ca07f4dfc
ZakharSytoi <zakharsytoi@gmail.com>
2024-04-29 15:41:56 +0200

    fix minor spelling mistakes

--- backend/src/main/java/com/example/course_project_2023/service/dto/UserRegisterDtoRequest.java
+++ backend/src/main/java/com/example/course_project_2023/service/dto/UserRegisterDtoRequest.java
@@ -7,14 +7,14 @@ import jakarta.validation.constraints.NotNull;
 import org.hibernate.validator.constraints.Length;
 
 public record UserRegisterDtoRequest(
-        @NotNull(message = "Nickname can not be Null")
+        @NotNull(message = "nickname can not be Null")
         @Length(min = 6, max = 30, message = "nickname length must be between 6 and 30 characters")
         String nickname,
-        @NotNull(message = "UniversityId can not be Null")
+        @NotNull(message = "universityId can not be Null")
         @Min(value = 0, message = "universityId value must be greater then or equal 0")
         @Max(value = 10000, message = "universityId value must not exceed 10000")
         Long universityId,
-        @NotNull(message = "fieldOdStudies can not be null")
+        @NotNull(message = "fieldOfStudies can not be null")
         @Length(min = 3, max = 50, message = "fieldOfStudies length must be between 6 and 30 characters")
         String fieldOfStudies,
         @NotNull(message = "email can not be null")
