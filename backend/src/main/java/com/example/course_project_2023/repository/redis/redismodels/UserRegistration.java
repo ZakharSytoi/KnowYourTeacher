94d1f24254855677482fe968b74df6be9031ddd6
ZakharSytoi <zakharsytoi@gmail.com>
2024-05-30 18:59:34 +0200

    implement user registration entity

---
+++ backend/src/main/java/com/example/course_project_2023/repository/redis/redismodels/UserRegistration.java
@@ -0,0 +1,32 @@
+package com.example.course_project_2023.repository.redis.redismodels;
+
+import com.example.course_project_2023.service.dto.UserRegisterDtoRequest;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Getter
+@RedisHash("userRegistration")
+public class UserRegistration {
+    @Id
+    private String id;
+    private String nickname;
+    private Long universityId;
+    private String fieldOfStudies;
+    private String email;
+    private String password;
+
+    public UserRegistration(UserRegisterDtoRequest dtoRequest){
+        this.nickname = dtoRequest.nickname();
+        this.universityId = dtoRequest.universityId();
+        this.fieldOfStudies = dtoRequest.fieldOfStudies();
+        this.email = dtoRequest.email();
+        this.password = dtoRequest.password();
+    }
+}
