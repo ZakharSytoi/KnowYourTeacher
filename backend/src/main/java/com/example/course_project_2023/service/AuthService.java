c333a339e2860081ca363d4971db960534fea3b4
ZakharSytoi <zakharsytoi@gmail.com>
2024-05-30 19:02:58 +0200

    implement registration activation functionality

--- backend/src/main/java/com/example/course_project_2023/service/AuthService.java
+++ backend/src/main/java/com/example/course_project_2023/service/AuthService.java
@@ -4,10 +4,13 @@ import com.example.course_project_2023.repository.daos.UniversityRepository;
 import com.example.course_project_2023.repository.daos.UserRepository;
 import com.example.course_project_2023.repository.model.SecurityUser;
 import com.example.course_project_2023.repository.model.User;
+import com.example.course_project_2023.repository.redis.redismodels.UserRegistration;
+import com.example.course_project_2023.repository.redis.redisrepos.UserRegistrationRepository;
 import com.example.course_project_2023.service.dto.UserLoginDtoRequest;
 import com.example.course_project_2023.service.dto.UserRegisterDtoRequest;
 import com.example.course_project_2023.service.exception.UserWithEmailAlreadyExistsException;
 import com.example.course_project_2023.service.exception.UserWithNicknameAlreadyExistsException;
+import com.example.course_project_2023.service.exception.notFound.ProfileNotFoundException;
 import com.example.course_project_2023.service.security.UserDetailServiceImpl;
 import com.example.course_project_2023.service.security.util.JwtUtil;
 import jakarta.transaction.Transactional;
@@ -23,6 +26,7 @@ import java.util.Set;
 @Component
 @RequiredArgsConstructor
 public class AuthService {
+    private final UserRegistrationRepository userRegistrationRepository;
     private final AuthenticationManager authenticationManager;
     private final JwtUtil jwtUtil;
     private final UserDetailServiceImpl userDetailService;
@@ -32,7 +36,8 @@ public class AuthService {
     private final UserRepository userRepository;
     private final UniversityRepository universityRepository;
     private final UserService userService;
-    public String authenticateUser(UserLoginDtoRequest userLoginDtoRequest){
+
+    public String authenticateUser(UserLoginDtoRequest userLoginDtoRequest) {
         authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(
                         userLoginDtoRequest.getUsername(),
@@ -43,28 +48,47 @@ public class AuthService {
                 userLoginDtoRequest.getUsername());
         return jwtUtil.generateToken(userDetails);
     }
+
     @Transactional
     public void registerUser(UserRegisterDtoRequest request) throws UserWithEmailAlreadyExistsException, UserWithNicknameAlreadyExistsException {
-        if(securityUserService.findByEmail(request.email()).isPresent()){
+        if (securityUserService.findByEmail(request.email()).isPresent() || userRegistrationRepository.existsByEmail(request.email())) {
             throw new UserWithEmailAlreadyExistsException(String.format("User with email %s already exists.", request.email()));
         }
-        if(userService.existsWithNickname(request.nickname())){
+        if (userService.existsWithNickname(request.nickname()) || userRegistrationRepository.existsByNickname(request.nickname())) {
             throw new UserWithNicknameAlreadyExistsException(String.format("User with nickname %s already exists.", request.nickname()));
         }
+
+        UserRegistration userRegistration = userRegistrationRepository.save(new UserRegistration(request));
+        System.out.println(userRegistration.getId());
+    }
+
+    public void activateProfile(String id) {
+        UserRegistration registration = userRegistrationRepository.findById(id)
+                .orElseThrow(
+                        () -> new ProfileNotFoundException("There is no such profile to be activated")
+                );
+        UserRegisterDtoRequest registrationRequest = new UserRegisterDtoRequest(
+                registration.getNickname(),
+                registration.getUniversityId(),
+                registration.getFieldOfStudies(),
+                registration.getEmail(),
+                registration.getPassword()
+        );
         User user = new User();
-        user.setNickname(request.nickname());
-        user.setUniversity(universityRepository.findById(request.universityId()).get());
-        user.setFieldOfStudies(request.fieldOfStudies());
+        user.setNickname(registrationRequest.nickname());
+        user.setUniversity(universityRepository.findById(registrationRequest.universityId()).get());
+        user.setFieldOfStudies(registrationRequest.fieldOfStudies());
 
         SecurityUser securityUser = new SecurityUser();
-        securityUser.setEmail(request.email());
-        securityUser.setPassword(passwordEncoder.encode(request.password()));
+        securityUser.setEmail(registrationRequest.email());
+        securityUser.setPassword(passwordEncoder.encode(registrationRequest.password()));
         securityUser.setRoles(Set.of(roleService.findRoleByName("USER")));
 
         user.setSecurityUser(securityUser);
         securityUser.setUser(user);
 
         userRepository.save(user);
+        userRegistrationRepository.deleteById(id);
 
     }
 }
