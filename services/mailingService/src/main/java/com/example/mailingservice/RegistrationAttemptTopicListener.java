b0d4d589b6be2b17d31287045c78e7f43828ad15
ZakharSytoi <zakharsytoi@gmail.com>
2024-06-01 14:36:29 +0200

    make changes to listener in order to accept registration attempt objects

--- services/mailingService/src/main/java/com/example/mailingservice/RegistrationAttemptTopicListener.java
+++ services/mailingService/src/main/java/com/example/mailingservice/RegistrationAttemptTopicListener.java
@@ -1,12 +1,13 @@
 package com.example.mailingservice;
 
+import com.example.mailingservice.model.RegistrationAttempt;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RegistrationAttemptTopicListener {
     @KafkaListener(id = "RegistrationAttemptTopicListener", topics = "kyt.registration.registration-attempted")
-    public void listen(String id) {
-        System.out.println(id);
+    public void listen(RegistrationAttempt registrationAttempt) {
+        System.out.println(registrationAttempt);
     }
 }
