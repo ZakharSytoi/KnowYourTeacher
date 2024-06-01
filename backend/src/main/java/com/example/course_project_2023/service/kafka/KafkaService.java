44694a2b3e11042e24a56254da4d19f61abe29ec
ZakharSytoi <zakharsytoi@gmail.com>
2024-06-01 14:31:26 +0200

    modify kafka service in order to send registration attempt objects

--- backend/src/main/java/com/example/course_project_2023/service/kafka/KafkaService.java
+++ backend/src/main/java/com/example/course_project_2023/service/kafka/KafkaService.java
@@ -1,5 +1,6 @@
 package com.example.course_project_2023.service.kafka;
 
+import com.example.course_project_2023.service.dto.kafka.RegistrationAttempt;
 import lombok.RequiredArgsConstructor;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -8,11 +9,11 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 public class KafkaService {
-    private final KafkaTemplate<String, String> kafkaTemplate;
+    private final KafkaTemplate<String, RegistrationAttempt> kafkaTemplate;
 
-    public void sendRegistrationAttemptMessage(final String message) {
-        final ProducerRecord<String, String> record =
-                new ProducerRecord<>("kyt.registration.registration-attempted", message);
+    public void sendRegistrationAttemptMessage(final RegistrationAttempt registrationAttempt) {
+        final ProducerRecord<String, RegistrationAttempt> record =
+                new ProducerRecord<>("kyt.registration.registration-attempted", registrationAttempt);
 
         kafkaTemplate.send(record);
     }
