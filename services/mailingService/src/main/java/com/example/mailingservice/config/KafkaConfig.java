c79a73e45baf20665b2996cc90ee4034b7719929
ZakharSytoi <zakharsytoi@gmail.com>
2024-06-01 14:33:39 +0200

    add jackson dependencies in order to receive registration attempt objects

--- services/mailingService/src/main/java/com/example/mailingservice/config/KafkaConfig.java
+++ services/mailingService/src/main/java/com/example/mailingservice/config/KafkaConfig.java
@@ -1,5 +1,6 @@
 package com.example.mailingservice.config;
 
+import com.example.mailingservice.model.RegistrationAttempt;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,6 +10,7 @@ import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -21,7 +23,7 @@ public class KafkaConfig {
     private String bootstrapAddress;
 
     @Bean
-    public ConsumerFactory<String, String> consumerFactory() {
+    public ConsumerFactory<String, RegistrationAttempt> consumerFactory() {
         Map<String, Object> props = new HashMap<>();
         props.put(
                 ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
@@ -31,15 +33,17 @@ public class KafkaConfig {
                 StringDeserializer.class);
         props.put(
                 ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
-                StringDeserializer.class);
-        return new DefaultKafkaConsumerFactory<>(props);
+                JsonDeserializer.class);
+        props.put(JsonDeserializer.TYPE_MAPPINGS, "com.example.course_project_2023.service.dto.kafka.RegistrationAttempt:com.example.mailingservice.model.RegistrationAttempt");
+        props.put(JsonDeserializer.TRUSTED_PACKAGES, "com.example.course_project_2023.service.dto.kafka");
+        return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(), new JsonDeserializer<>(RegistrationAttempt.class));
     }
 
     @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, String>
+    public ConcurrentKafkaListenerContainerFactory<String, RegistrationAttempt>
     kafkaListenerContainerFactory() {
 
-        ConcurrentKafkaListenerContainerFactory<String, String> factory =
+        ConcurrentKafkaListenerContainerFactory<String, RegistrationAttempt> factory =
                 new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory());
         return factory;
