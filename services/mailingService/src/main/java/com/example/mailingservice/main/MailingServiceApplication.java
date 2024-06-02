412e6c2d2dbc98006840532d12818a30d650d60f
ZakharSytoi <zakharsytoi@gmail.com>
2024-06-02 15:34:32 +0200

    add component scan for proper configuration

--- services/mailingService/src/main/java/com/example/mailingservice/main/MailingServiceApplication.java
+++ services/mailingService/src/main/java/com/example/mailingservice/main/MailingServiceApplication.java
@@ -1,10 +1,12 @@
-package com.example.mailingservice;
+package com.example.mailingservice.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.example.mailingservice")
 public class MailingServiceApplication {
 
     public static void main(String[] args) {
