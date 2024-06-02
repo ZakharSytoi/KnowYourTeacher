ca21fc72ad1f3fd55085dd76aa32b048f038aca8
ZakharSytoi <zakharsytoi@gmail.com>
2024-06-02 15:28:15 +0200

    add nickname field to registration attempt

--- backend/src/main/java/com/example/course_project_2023/service/dto/kafka/RegistrationAttempt.java
+++ backend/src/main/java/com/example/course_project_2023/service/dto/kafka/RegistrationAttempt.java
@@ -1,6 +1,7 @@
 package com.example.course_project_2023.service.dto.kafka;
 
 public record RegistrationAttempt(
+        String nickname,
         String email,
         String id
 ) {
--- services/mailingService/src/main/java/com/example/mailingservice/model/RegistrationAttempt.java
+++ services/mailingService/src/main/java/com/example/mailingservice/model/RegistrationAttempt.java
@@ -1,6 +1,7 @@
 package com.example.mailingservice.model;
 
 public record RegistrationAttempt(
+        String nickname,
         String email,
         String id
 ) {
